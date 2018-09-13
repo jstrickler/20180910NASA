a3b9c7f5427999e11eea6ee4d5dfb6f98bbbc1c0
jstrickler <jstrickler@gmail.com>
2018-09-13 14:15:03 -0500

    cython numba etc

---
+++ numba_primes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import numba
+
+@numba.jit() # <1>
+def primes(kmax):
+    p = [0] * kmax
+    result = []
+    k = 0
+    n = 2
+    while k < kmax:
+        i = 0
+        while i < k and n % p[i] != 0:
+            i = i + 1
+        if i == k:
+            p[k] = n
+            k = k + 1
+            result.append(n)
+        n = n + 1
+    return result
+
